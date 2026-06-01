54ef30976efd8aed3e0742c3c69cc9bf346e9d9d
Hector_B <112621010+Hectorbdv@users.noreply.github.com>
2026-06-01 02:44:17 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,37 @@
-const CACHE_NAME = "daily-math-v2";
-
+const CACHE_NAME = "daily-math-v3";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json",
   "./service-worker.js",
-  "./icon.png"
+  "./icon.png",
+  "./favicon.ico",
+  "./favicon-16.png",
+  "./favicon-32.png",
+  "./apple-touch-icon.png",
+  "./fonts/ibm-plex-sans-v23-latin-300.woff2",
+  "./fonts/ibm-plex-sans-v23-latin-regular.woff2",
+  "./fonts/ibm-plex-sans-v23-latin-500.woff2",
+  "./fonts/inter-v20-latin-300.woff2",
+  "./fonts/inter-v20-latin-regular.woff2",
+  "./fonts/inter-v20-latin-500.woff2",
+  "./fonts/libre-franklin-v20-latin-300.woff2",
+  "./fonts/libre-franklin-v20-latin-regular.woff2",
+  "./fonts/libre-franklin-v20-latin-500.woff2",
+  "./fonts/roboto-v51-latin-300.woff2",
+  "./fonts/roboto-v51-latin-regular.woff2",
+  "./fonts/roboto-v51-latin-500.woff2",
+  "./fonts/source-sans-3-v19-latin-300.woff2",
+  "./fonts/source-sans-3-v19-latin-regular.woff2",
+  "./fonts/source-sans-3-v19-latin-500.woff2"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
   );
-
-  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
@@ -21,19 +39,16 @@ self.addEventListener("activate", event => {
     caches.keys().then(cacheNames =>
       Promise.all(
         cacheNames
-          .filter(cacheName => cacheName !== CACHE_NAME)
-          .map(cacheName => caches.delete(cacheName))
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
       )
     )
   );
-
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      return cachedResponse || fetch(event.request);
-    })
+    caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
